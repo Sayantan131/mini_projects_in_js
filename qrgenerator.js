183cd46ed2952798008a6b0ecd6059ab9496f8d4
Sayantan <maitysayantan111@gmail.com>
2023-09-22 12:56:21 +0530

    running

--- qrgenerator.js
+++ qrgenerator.js
@@ -6,4 +6,5 @@ let qrImage = document.getElementById("qrImage");
 
 function qrGenerate(){
     qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value;
+    imgBox.classList.add("show-img");
 }
