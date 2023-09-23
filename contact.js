f224d9b40bb3f508d89ae6b1074585d9487dd50b
Sayantan <maitysayantan111@gmail.com>
2023-09-23 23:21:59 +0530

    running

--- contact.js
+++ contact.js
@@ -1,3 +1,14 @@
-function send(){
+function send() {
     console.log("function call");
+    Email.send({
+        Host: "smtp.elasticemail.com",
+        Username: "maitysayantan67@gmail.com",
+        Password: "76C139FB4D9A675559F1A3A5C6AA40EEAF4A",   //backup-code fot smtp- 555788
+        To: 'maitysayantan111@gmail.com',
+        From: "maitysayantan67@gmail.com",
+        Subject: "This is the subject",
+        Body: "And this is the body"
+    }).then(
+        message => alert(message)
+    );
 }
