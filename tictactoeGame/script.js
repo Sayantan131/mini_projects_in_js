8576d635c7c070a691530740ea4ee0ac5fbde736
Sayantan <maitysayantan111@gmail.com>
2023-11-22 20:15:39 +0530

    running

--- tictactoeGame/script.js
+++ tictactoeGame/script.js
@@ -3,6 +3,7 @@ console.log("Welcome to Tic Tac Toe");
 let music = new Audio("music.mp3")
 let audioTurn = new Audio("ting.mp3")
 let gameover = new Audio("gameover.mp3")
+let isgameover = false;
 
 let turn = "X";
 
@@ -15,7 +16,27 @@ const changeTurn = ()=>{
 
 //Function to check the win
 const checkWin = ()=>{
-    
+   let boxtext = document.getElementsByClassName('boxtext');
+    let wins = [
+      [0,1,2],
+      [3,4,5],
+      [6,7,8],
+      [0,3,6],
+      [1,4,7],
+      [2,5,8],
+      [0,4,8],
+      [2,4,6],
+    ]
+    wins.forEach(e =>{
+      if((boxtext[e[0]].innerText === boxtext[e[1]].innerText) && (boxtext[e[2]].innerText === boxtext[e[1]].innerText ) && (boxtext[e[0]].innerText !=='')){
+         document.querySelector('.info').innerText = boxtext[e[0]].innerText + "won";
+         isgameover = true;
+         gameover.play();
+         document.getElementsByClassName('imgbox').getElementByTagName('img').style.width = "200px";
+         
+         }
+         
+    })
 }
 
 let boxes = document.getElementsByClassName("box");
@@ -28,7 +49,10 @@ Array.from(boxes).forEach(element=>{
          turn = changeTurn();
          audioTurn.play();
          checkWin();
-        document.getElementsByClassName("info")[0].innerText = "Turn for " + turn;
+         if(!isgameover){
+            document.getElementsByClassName("info")[0].innerText = "Turn for " + turn;
+         }
+       
       //   console.log(document.getElementsByClassName("info")[0]);
 
       }
