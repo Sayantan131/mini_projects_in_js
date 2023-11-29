52f2110a99576717a6f4ae9f6f78118e24df1c4c
Sayantan <maitysayantan111@gmail.com>
2023-11-29 19:44:06 +0530

    done

--- Faulty_Calculator/index.js
+++ Faulty_Calculator/index.js
@@ -18,3 +18,26 @@ It performs wrong operations 10% of the times
 
 
 */
+
+let random = Math.random();
+let a =  prompt("Enter First Number: ")
+let b = prompt("Enter Second Number: ")
+let c = prompt("Enter operation: ")
+
+let obj={
+    "+":"-",
+    "*":"+",
+    "-":"/",
+    "/":"**",
+}
+
+
+
+if(random > 0.1){
+    alert(`The result is: ${eval(`${a} ${c} ${b}`)}`)
+   
+}
+else{
+    c=obj(c);
+    alert(`The result is: ${eval(`${a} ${c} ${b}`)}`)
+}
