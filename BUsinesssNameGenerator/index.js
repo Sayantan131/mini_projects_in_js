f71ddd35ffc427b4db004811e40faa50b5d305ad
Sayantan <maitysayantan111@gmail.com>
2023-11-29 22:05:57 +0530

    name generator using js

---
+++ BUsinesssNameGenerator/index.js
@@ -0,0 +1,60 @@
+/* Create a business name generator by combining 
+list of adjectives and shop name and another word
+
+
+Adjectives:
+Crazy
+Amazing
+Fire
+
+Shop Name:
+Engine
+Foods
+Garments
+
+Another Word:
+Bros
+Limited
+Hub
+*/
+
+let rand = Math.random();
+
+let first,second,third
+
+if(rand<0.33){
+    first = "Crazy";
+}
+else if(rand<0.66 && rand>0.33){
+    first = "Amazing";
+}
+else{
+    first="Fire";
+}
+ rand = Math.random();
+if(rand<0.33){
+   second = "Engine";
+}
+else if(rand<0.66 && rand>0.33){
+    second = "Foods";
+}
+else{
+    second ="Garments";
+}
+rand = Math.random();
+if(rand<0.33){
+   third = "Bros";
+}
+else if(rand<0.66 && rand>0.33){
+    third  = "Limited";
+}
+else{
+    third  ="Hub";
+}
+
+console.log(`${first} ${second} ${third}`)
+
+
+
+
+
