12cbad591cbd89d60d0ece4fafe8a050b4e37a61
Sayantan <maitysayantan111@gmail.com>
2023-11-29 18:03:30 +0530

    mini project on Faulty Calculator

---
+++ Faulty_Calculator/index.js
@@ -0,0 +1,20 @@
+/* Create a faulty calculator using JavaScript 
+
+This faulty calculator does following:
+
+1. It takes two numbers as input from the user
+2. It perfors Wrong operations as follows 
+
+
++ ----> -
+* ----> +
+- ----> /
+/ ----> **
+
+
+It performs wrong operations 10% of the times
+
+
+
+
+*/
