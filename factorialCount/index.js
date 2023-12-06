fc450a06f029cfa4dee5bf9331aa61b848bd73c5
Sayantan <maitysayantan111@gmail.com>
2023-12-06 22:49:28 +0530

    factorial using reduce method

--- factorialCount/index.js
+++ factorialCount/index.js
@@ -1,13 +1,32 @@
-let number = prompt("Enter the number you want factorial: ") ;
-let fact = 1
+// let number = prompt("Enter the number you want factorial: ") ;
 
- function factorial(number){
+// let fact = 1
 
-    for (let index = 1; index <= number; index++) {
+//  function factorial(number){
+
+//     for (let index = 1; index <= number; index++) {
         
-        fact = fact*index;
-    }
-    return fact;
+//         fact = fact*index;
+//     }
+//     return fact;
+// }
+// alert(`so the factorial of ${number} is: ${factorial(number)}`);
+
+
+
+
+//using redu e method
+
+let number = 6;
+
+function factorial(number){
+    let arr = Array.from(Array(number+1).keys())
+    // console.log(arr.slice(1,));
+    let c = arr.slice(1,).reduce((a,b)=>a*b)
+    // console.log(c);
+    return c;
 }
 
-alert(`so the factorial of ${number} is: ${factorial(number)}`);
+console.log(factorial(number));
+
+
