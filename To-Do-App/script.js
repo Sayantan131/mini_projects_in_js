a705be5b298322d1b39230689c94820817040ac4
Sayantan <maitysayantan111@gmail.com>
2023-12-27 11:19:44 +0530

    js starting

---
+++ To-Do-App/script.js
@@ -0,0 +1,19 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById('list-container');
+
+function addTask(){
+    
+    if(inputBox.value ===''){
+        alert("you have to enter something");
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7"
+       li.appendChild(span);
+    }
+     
+    inputBox.value = "";
+}
