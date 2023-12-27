031f66c8d9ab5e08b7c5eeadde3e00290ba4bc9c
Sayantan <maitysayantan111@gmail.com>
2023-12-27 12:00:16 +0530

    project done

--- To-Do-App/script.js
+++ To-Do-App/script.js
@@ -16,4 +16,27 @@ function addTask(){
     }
      
     inputBox.value = "";
+    saveData();
 }
+
+listContainer.addEventListener("click",function(e){
+if(e.target.tagName === "LI"){
+    e.target.classList.toggle("checked");
+    saveData()
+}
+else if(e.target.tagName === "SPAN"){
+    e.target.parentElement.remove();
+    saveData()
+}
+
+},false)
+
+function saveData(){
+    localStorage.setItem("data",listContainer.innerHTML);
+}
+
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+
+showTask();
