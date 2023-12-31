8acb74e9218da6de64acedd7995a2db27ee9e5d0
Sayantan <maitysayantan111@gmail.com>
2023-12-31 09:25:49 +0530

    due date feature added

--- To-Do-App/script.js
+++ To-Do-App/script.js
@@ -2,45 +2,49 @@ const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById('list-container');
 const dateInput = document.getElementById("dateInput");
 
-function addTask(){
-    
-    if(inputBox.value ===''){
+function addTask() {
+
+    if (inputBox.value === '') {
         alert("you have to enter something");
     }
-    else{
+    else {
         let li = document.createElement("li");
         li.innerHTML = inputBox.value;
         listContainer.appendChild(li);
         let date = document.createElement("p");
         date.innerHTML = dateInput.value;
         li.appendChild(date);
+        let dueDate = document.createElement("h4");
+        dueDate.innerHTML = "Due Date: " + dueDateInput.value; // Display "Due Date:" for clarity
+        li.appendChild(dueDate);
+
         let span = document.createElement("span");
         span.innerHTML = "\u00d7"
-       li.appendChild(span);
+        li.appendChild(span);
     }
-     
+
     inputBox.value = "";
     dateInput.value = "";
     saveData();
 }
 
-listContainer.addEventListener("click",function(e){
-if(e.target.tagName === "LI"){
-    e.target.classList.toggle("checked");
-    saveData()
-}
-else if(e.target.tagName === "SPAN"){
-    e.target.parentElement.remove();
-    saveData()
+listContainer.addEventListener("click", function (e) {
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+        saveData()
+    }
+    else if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+        saveData()
+    }
+
+}, false)
+
+function saveData() {
+    localStorage.setItem("data", listContainer.innerHTML);
 }
 
-},false)
-
-function saveData(){
-    localStorage.setItem("data",listContainer.innerHTML);
-}
-
-function showTask(){
+function showTask() {
     listContainer.innerHTML = localStorage.getItem("data");
 }
 
